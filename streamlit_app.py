258cfb14d725abbed3e8fd255f757f1e8be93878
desild <dsilvadesil@gmail.com>
2024-12-05 13:01:43 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,14 @@
 
+import streamlit as st
+from scrape import scrape_website
+
+
+st.title("AI Web Scrapper")
+url=st.text_input("Enter the website url:")
+
+
+if st.button("scrape site"):
+    st.write("Scrapping the website")
+    result=scrape_website(url)
+    print(result)
+    
